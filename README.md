4627d9da41a51d6b6e31e0f76ac40252993c5d3b
Matt Rutkowski <mrutkows@us.ibm.com>
2017-06-09 13:50:23 -0500

    Update files to include ASF headers and fix scan errors.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-#OpenWhisk Package for Jira 
+# OpenWhisk Package for Jira
+
+This package includes actions that interact with JIRA software
+software development tool used for issue tracking, and project
+management functions.
+
+For more information on JIRA vist their home page:
+https://www.atlassian.com/software/jira
+
+Specifically, this package includes:
+
+## Actions
+
+* "createIssue"
+    * JavaScript: [createIssue.js](https://github.com/apache/incubator-openwhisk-package-jira/blob/master/src/jirajs/createIssue.js)
+    * Swift: [createIssue.swift](https://github.com/apache/incubator-openwhisk-package-jira/blob/master/src/jiraswift/createIssue.swift)
--- src/jiraswift/createIssue.swift
+++ src/jiraswift/createIssue.swift
@@ -1,8 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /***********************************************************
- 
+
  Create Issue in Jira.  Issue is encoded as a JSON object
  in the form:
- 
+
  {
  "fields": {
  "project":
@@ -16,13 +33,13 @@
  }
  }
  }
- 
+
  For project, key is the key value assigned to a Jira project, e.g. "MyAwesomeScrumProject"
  may have the key "MYAW"
- 
+
  @param issue the jsonObject representing the issue
  @return response from Jira representing succes or failure
- 
+
  ***********************************************************/
 import Foundation
 import Dispatch
@@ -30,35 +47,35 @@ import KituraNet
 import KituraSys
 
 func main(args: [String:Any]) -> [String:Any] {
-    
+
     guard let username = args["jiraUsername"] as? String else {
         return ["error":"Jira username not set"]
     }
-    
+
     guard let password = args["jiraPassword"] as? String else {
         return ["error": "Jira password not set"]
     }
-    
+
     guard let host = args["jiraHost"] as? String else {
         return ["error": "Jira host not set"]
-        
+
     }
-    
+
     let loginString = ("\(username):\(password)")
-    
+
     let loginData: NSData = loginString.data(using: NSUTF8StringEncoding)!
     let base64LoginString = loginData.base64EncodedString(NSDataBase64EncodingOptions(rawValue: 0))
-    
+
     let headers = ["Content-Type" : "application/json",
                    "Authorization" : "Basic \(base64LoginString)"]
-    
+
     let requestOptions = [ClientRequestOptions.schema("https://"),
                           ClientRequestOptions.method("post"),
                           ClientRequestOptions.hostname(host),
                           ClientRequestOptions.path("/rest/api/2/issue/"),
                           ClientRequestOptions.headers(headers),
                           ClientRequestOptions.disableSSLVerification]
-    
+
     let request = HTTP.request(requestOptions) { response in
         if response != nil {
             do {
@@ -66,13 +83,13 @@ func main(args: [String:Any]) -> [String:Any] {
                 // the response as NSData
                 let jsonData = NSMutableData()
                 try response!.readAllData(into: jsonData)
-                
+
                 let resp = try NSJSONSerialization.jsonObject(with: jsonData, options: [])
                 let r = resp as! [String:Any]
-                
+
                 print("Got response \(r)")
-                
-                
+
+
             } catch {
                 print("error Could not parse a valid JSON response.")
             }
@@ -80,26 +97,22 @@ func main(args: [String:Any]) -> [String:Any] {
             print("error Did not receive a response.")
         }
     }
-    
+
     do {
         #if os(OSX)
             let jsonData = try NSJSONSerialization.data(withJSONObject: args as! [String:AnyObject], options: [])
         #elseif os(Linux)
             let jsonData = try NSJSONSerialization.data(withJSONObject: args.bridge(), options: [])
         #endif
-        
-        
-        
+
+
+
         request.write(from: jsonData)
         request.end()
-        
+
     } catch {
         print("JSON Error \(error)")
     }
-    
+
     return args
 }
-
-
-
-
